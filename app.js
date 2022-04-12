2e8169c80ea1fe8da2fe3cfdab43c3c03fe95d4e
Marc Oller <marc.om@icloud.com>
2022-04-12 21:54:55 +0200

    feat: add radios tab

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('def-tab')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('def-tab')
+        })
+        tab.classList.add('def-tab')
+        target.classList.add('def-tab')
+    })
+})
